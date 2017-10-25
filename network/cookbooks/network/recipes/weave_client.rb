31356658338b5ebaedcb51599c09c793aaffb19c
Bogdan <metallica127@mail.ru>
2017-10-25 13:07:36 +0300

    weave client fx

--- network/cookbooks/network/recipes/weave_client.rb
+++ network/cookbooks/network/recipes/weave_client.rb
@@ -1,3 +1,31 @@
+service 'overlay' do
+  action [:disable, :stop]
+end
+
+service 'weave' do
+  action [:stop]
+end
+
+ruby_block 'delete overlay setup from rc.local' do
+  block do
+    file = Chef::Util::FileEdit.new('/etc/rc.local')
+    file.search_file_delete_line(/service overlay restart/)
+    file.search_file_delete_line(/brctl addbr overlay/)
+    file.search_file_delete_line(/ifconfig overlay up/)
+    file.search_file_delete_line(/ip addr add 51.1.0./)
+    file.write_file
+  end
+end
+
+
+bash 'stop overlay' do
+  code <<-EOH
+    ifdown overlay | true
+    brctl delbr overlay | true
+  EOH
+end
+
+
 
 template '/etc/systemd/system/weave.service' do
   cookbook 'network'
