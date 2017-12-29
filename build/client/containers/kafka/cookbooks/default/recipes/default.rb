c0324d416b5b81db38f089c2b1a61a80674d7fc3
Bogdan <bn.bliznyuk@gmail.com>
2017-12-29 12:07:56 +0200

    kafka cookbook

--- build/client/containers/kafka/cookbooks/default/recipes/default.rb
+++ build/client/containers/kafka/cookbooks/default/recipes/default.rb
@@ -6,18 +6,43 @@
   end
 end
 
-execute 'add Confluent key' do
-  command 'wget -qO - http://packages.confluent.io/deb/3.1/archive.key | apt-key add -'
+# execute 'add Confluent key' do
+#   command 'wget -qO - http://packages.confluent.io/deb/3.1/archive.key | apt-key add -'
+# end
+#
+# execute 'add Confluent repo' do
+#   command %Q(add-apt-repository "deb [arch=amd64] http://packages.confluent.io/deb/3.1 stable main")
+# end
+
+apt_repository 'confluent' do
+  uri 'http://packages.confluent.io/deb/3.1'
+  components ['main', 'stable']
+  arch 'amd64'
+  key 'http://packages.confluent.io/deb/3.1/archive.key'
 end
 
-execute 'add Confluent repo' do
-  command %Q(add-apt-repository "deb [arch=amd64] http://packages.confluent.io/deb/3.1 stable main")
+
+apt_repository 'java8' do
+  uri 'ppa:webupd8team/java'
 end
+
+
+bash 'java-licence-agree' do
+  code <<-EOH
+    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+  EOH
+end
+
+apt_repository 'java8' do
+  uri 'ppa:webupd8team/java'
+end
+
 
 # apt_update 'update'
-execute 'apt update' do
-  command 'apt-get update'
-end
+apt_update
+
+package 'java'
 
 package ['confluent-platform-oss-2.10', 'ntp'] do
   action :install
